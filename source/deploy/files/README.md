8e1f1be57238c437cb994e20f8748edae4e8d4d2
Dinesh Waghmare <rdn.v03@gmail.com>
2017-12-16 07:31:32 +0530

    Drupal 7 Demo

---
+++ source/deploy/files/README.md
@@ -0,0 +1,1 @@
+Place Public Files Here
---
+++ source/deploy/local.settings.php
@@ -0,0 +1,11 @@
+
+<?php
+
+// Disable compression
+$conf['preprocess_js'] = 1;
+$conf['preprocess_css'] = 1;
+
+// Set file system paths
+$conf['file_public_path'] = 'sites/default/files';
+$conf['file_private_path'] = 'sites/default/files/private';
+$conf['file_temporary_path'] = 'sites/default/files/tmp';
